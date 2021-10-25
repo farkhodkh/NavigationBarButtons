87da638f44143a2b19b3fe6a6ebc40c4fc042934
farkhod <986532qw@gmail.com>
2021-10-25 20:06:57 +0300

    Navigation bar buttons setup and color

--- NavigationBarButtons/ContentView.swift
+++ NavigationBarButtons/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var companyName: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                ProfileImages(imageName: "foto")
+                
+                Form{
+                    Section(header: Text("Personal Info")) {
+                        TextField("First name", text: $firstName)
+                        TextField("Last name", text: $lastName)
+                        TextField("Company name", text: $companyName)
+                    }
+                }
+            }
+            .navigationTitle("Profile")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        print("Saved")
+                    }
+                    
+                    Button {
+                        print("Saved")
+                    } label: {
+                        Label("Save", systemImage: "square.and.arrow.down")
+                    }
+                }
+            }
+        }
+        .accentColor(.green)
+    }
+}
+
+struct ProfileImages: View {
+    var imageName: String
+    
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .frame(width: 100, height: 100)
+            .clipShape(Circle())
     }
 }
 
