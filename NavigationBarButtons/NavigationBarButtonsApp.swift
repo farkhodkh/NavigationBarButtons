4a62a5255f4419a3ca806fd8a39e0bbf2fe71c04
farkhod <986532qw@gmail.com>
2021-10-25 19:44:47 +0300

    Initial Commit

---
+++ NavigationBarButtons/NavigationBarButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBarButtonsApp.swift
+//  NavigationBarButtons
+//
+//  Created by Farkhod on 25.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationBarButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
